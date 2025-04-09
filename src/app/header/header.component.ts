912cb4a849b1fb6c0b7f81caac2efcd3ca444f6e
Daniel Paz <you@example.com>
2025-04-09 11:55:49 -0600

    si

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HeaderService } from '../services/header-service/header.service';
+import { Header } from '../models/header/header.model';
+import { map } from 'rxjs/operators';
+import { DocumentChangeAction } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  header!: Header;
 
+  constructor(public headerService: HeaderService) {
+    this.headerService.getHeader().snapshotChanges().pipe(
+      map((changes: DocumentChangeAction<Header>[]) =>
+        changes.map((c: DocumentChangeAction<Header>) => ({
+          id: c.payload.doc.id,
+          ...c.payload.doc.data() as Header
+        }))
+      )
+    ).subscribe((data: Header[]) => {
+      this.header = data[0];
+      console.log(this.header);
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5gQ0Y5gdRulDT3BCBTfI9dwPkd7loXxY",
+    authDomain: "my-cv-f90b9.firebaseapp.com",
+    projectId: "my-cv-f90b9",
+    storageBucket: "my-cv-f90b9.firebasestorage.app",
+    messagingSenderId: "840047044308",
+    appId: "1:840047044308:web:0adcf27db5ec1f7b353d41"
+  }
+};
+
